e9cfc43fd38ed8ace9b3ab40225ce483bff8cc73
vinothvk <vinothvk@gmail.com>
2018-12-02 22:28:55 -0800

    change5

--- internals/scripts/npmcheckversion.js
+++ internals/scripts/npmcheckversion.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 exec('npm -v', (err, stdout) => {
   if (err) throw err;
-  if (parseFloat(stdout) < 5) {
+  if (parseFloat(stdout) < 6) {
     // NOTE: This can happen if you have a dependency which lists an old version of npm in its own dependencies.
     throw new Error(`[ERROR] You need npm version @>=5 but you have ${stdout}`);
   }
